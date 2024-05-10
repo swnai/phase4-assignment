aea4add6ef8393a81259ded1d304388338478d7d
swnai <swnai@eg.dk>
2024-05-10 10:11:45 +0530

    added files

---
+++ index.js
@@ -0,0 +1,6 @@
+var a=document.createElement("p")
+a.innerHTML="Swastik";
+document.body.appendChild(a)
+document.body.insertAdjacentHTML('beforeend', '<p><h1>This is a new para element</h1></p>');
+var b=document.getElementById('body-block');
+b
